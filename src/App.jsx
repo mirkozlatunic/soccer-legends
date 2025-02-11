b15465448901ea2aa989816ae00cc76cadba4da7
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-02-10 22:56:03 -0800

    create main structre for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from './components/Header';
 import './App.css';
+import Hero from './components/Hero';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <Main />
+      <Footer />
     </>
   );
 }
