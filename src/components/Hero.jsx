85bd83316d904321e0634c3b35d5ab930212d1cc
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-02-11 20:26:57 -0800

    Title and description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <h1>Welcome to Soccer Legends</h1>
+      <p>
+        Find your Soccer Legends in the World - Create your own stack of Legends
+      </p>
+    </div>
+  );
 }
 
 export default Hero;
