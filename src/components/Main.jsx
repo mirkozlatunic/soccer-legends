86da2a82f6950817306f5e55d89084afed4c0e97
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-02-22 18:04:06 -0800

    add sample card to Main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
+import MediaCard from './MediaCard';
 import './Main.css';
 
 function Main() {
-  return <div className="main">Main</div>;
+  return (
+    <div className="main">
+      <MediaCard />
+    </div>
+  );
 }
 
 export default Main;
